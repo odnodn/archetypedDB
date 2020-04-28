2caf6118dd6380e8e49d8767ff85f34fdffea7ee
ana@ana <ana@ana>
2020-04-27 21:41:56 -0300

    Adicionando cada elemento do arquétipo em um map

--- src/generatingRelationalDB.java
+++ src/generatingRelationalDB.java
@@ -1,6 +1,6 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -11,46 +11,50 @@ import org.w3c.dom.NodeList;
 
 public class generatingRelationalDB {
 
-  public static void main(String[] args) {
+
+  static Map<String, Map<String, String>> getArquetypeElements(String arquetypePath){
+    Map<String, String> element = new HashMap<String, String>();
+    Map<String, Map<String, String>> elements = new HashMap<String, Map<String, String>>();
     try {
-      File file = new File(
-          "/home/ana/Documentos/phd/archetypedDB/docs/archetypes/openEHR-EHR-OBSERVATION.condition_screening.v0.xml");
-      List<String> elements = new ArrayList<String>();
-      List<Object> elementsObj = new ArrayList<Object>();
+      File file = new File(arquetypePath);
+      //List<String> elements = new ArrayList<String>();
+      //List<Object> elementsObj = new ArrayList<Object>();
       DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
       DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
       Document document = docBuilder.parse(file);
       document.getDocumentElement().normalize();
       System.out.println("Root element: " + document.getDocumentElement().getNodeName());
       NodeList nodeList = document.getElementsByTagName("archetype").item(0).getChildNodes();
-      // nodeList is not iterable, so we are using for loop
-      for (int itr = 0; itr < nodeList.getLength(); itr++) {
-        Node node = nodeList.item(itr);
-        System.out.println("\nNode Name :" + node.getNodeName());
+       for (int i = 0; i < nodeList.getLength(); i++) {
+        Node node = nodeList.item(i);
         if (node.getNodeType() == Node.ELEMENT_NODE){
-          String content = document.getElementsByTagName(node.getNodeName()).item(0).getTextContent();
-          System.out.println("------------------ " + node.getNodeName() + " ----------------------");
-          System.out.println(content);
-          elements.add(content);
+          System.out.println("\nNode Name :" + node.getNodeName()); 
+          if(node.hasChildNodes()){
+            NodeList chilList = document.getElementsByTagName(node.getNodeName()).item(0).getChildNodes();
+            for (int j = 0; j < chilList.getLength(); j++) {
+              Node child = chilList.item(j);
+              if (child.getNodeType() == Node.ELEMENT_NODE){
+                String content = document.getElementsByTagName(child.getNodeName()).item(0).getTextContent();
+                System.out.println("------------------ " + child.getNodeName() + " ----------------------");
+                element.put(child.getNodeName(), content);
+              }
+            }
+            elements.put(node.getNodeName(), element);
+          }
         }
       }
+      System.out.println(elements.toString());
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return elements;
+  }
 
-      /*
-       * String originalLanguage =
-       * document.getElementsByTagName("original_language").item(0).getTextContent();
-       * elements.add(originalLanguage); String metadata =
-       * document.getElementsByTagName("description").item(0).getTextContent();
-       * elements.add(metadata); String terminology =
-       * document.getElementsByTagName("terminology_id").item(0).getTextContent();
-       * elements.add(terminology); String translations =
-       * document.getElementsByTagName("translations").item(0).getTextContent();
-       * elements.add(translations); String uid =
-       * document.getElementsByTagName("uid").item(0).getTextContent();
-       * elements.add(uid);
-       */
-
-      // System.out.println(metadata);
-
+  public static void main(String[] args) {
+    try {
+      String arquetypePath = "/home/ana/Documentos/phd/archetypedDB/docs/archetypes/openEHR-EHR-ADMIN_ENTRY.episode_institution.v0.xml";
+      Map<String, Map<String, String>> archetypeElements = getArquetypeElements(arquetypePath);
+      System.out.println(archetypeElements);
     } catch (Exception e) {
       System.out.println(e);
     }
