2f8f1575d4415ce49ba96e2732bc6c4ce76946e4
anapatriciagh <anapatricia3636@gmail.com>
2020-05-05 21:35:53 -0300

    Implementando conexao com o bd e o mapeamento das tabelas

--- src/archetypeUtil.js
+++ src/archetypeUtil.js
@@ -1,18 +1,69 @@
 const parser = require('xml2json');
 const fs = require('fs')
 const sequelize = require("../database/database")
+const { Sequelize, DataTypes } = require('sequelize');
 
+
+async function getColumn(element, key) {
+    let column = {}
+    column.name = key.toString()
+    let type
+    if (Array.isArray(element)) {
+        type = 'array'
+    } else {
+        type = typeof element
+    }
+    column.type = type
+    column.value = element
+    return column
+}
 
 module.exports = {
 
-    async createTables(archetypeObj) {
-        sequelize
-            .authenticate()
-            .then(() => {
-                console.log('Connection has been established successfully.');
-            })
-            .catch(err => {
-                console.error('Unable to connect to the database:', err);
-            });
+    async mappingTables(archetypeObj) {
+        let archetypeDetails = []
+        let archetypeMetadata = [] //metadados
+        let terminology = []
+        let itemTree = []
+        let itemTable = []
+        let column = {}
+        try {
+            //create archetype metadata table
+            for (const key in archetypeObj) {
+                if (archetypeObj.hasOwnProperty(key)) {
+                    const element = archetypeObj[key]
+                    if (key == 'definition' || key == 'description' || key == 'ontology') {
+                        archetypeObj1 = element
+                        for (const key1 in archetypeObj1) {
+                            if (archetypeObj1.hasOwnProperty(key1)) {
+                                const element1 = archetypeObj1[key1];
+                                column = await getColumn(element1, key1)
+                                if (key == 'definition') {
+                                    console.log('definition')
+                                } else if (key == 'description') {
+                                    archetypeDetails.push(column)
+                                } else if (key == 'ontology') {
+                                    terminology.push(column)
+                                }
+                            }
+                        }
+                    } else {
+                        column = await getColumn(element, key)
+                        archetypeMetadata.push(column)
+                    }
+
+                /* const table = sequelize.define(key, {
+
+                }); */
+            }
+        }
+        console.log(archetypeMetadata);
+        console.log(archetypeDetails);
+        console.log(terminology);
+
+        //console.log(User === sequelize.models.User);
+    } catch(error) {
+        console.log(error);
     }
+}
 }
--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
 const parser = require('xml2json')
-const fs = require('fs') 
+const fs = require('fs')
 const archetypeUtil = require("./archetypeUtil")
+const sequelize = require("../database/database")
 
+//Starting database connection
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 var xmlpath = "/home/ana/Documents/phd/projects/archetypedDB/docs/archetypes/openEHR-EHR-ADMIN_ENTRY.episode_institution.v0.xml"
 
-
-
-var archetypeObj
-
 fs.readFile(xmlpath, function (err, data) {
-    if (err) {
-        return console.log(err);
-    } else {
-      archetypeObj = JSON.parse(parser.toJson(data, { reversible: true }));
-      console.log(archetypeObj);
-    }
-}); 
+  if (err) {
+    return console.log(err);
+  } else {
+    archetypeObj = JSON.parse(parser.toJson(data, { reversible: true }));
+    console.log(archetypeObj);
+    archetypeObj = archetypeObj['archetype']
+    archetypeUtil.mappingTables(archetypeObj)
+  }
+});
 
